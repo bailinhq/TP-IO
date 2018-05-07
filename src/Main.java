8797f7d520c707bc680ebd2e4fc5a2d97049fbf3
hanniaAguilar <hannia.aguilar@ucr.ac.cr>
2018-05-06 22:14:26 -0600

    Se lee el número de clientes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int numero_Clientes = 0;
+
+        System.out.println("Introduczca el número de clientes que desea procesar");
+        numero_Clientes = reader.nextInt();
+
         Sistema sistema = new Sistema();
         while (true) {
             sistema.simular_sistema();
